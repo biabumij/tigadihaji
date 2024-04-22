11dade745fdeee8bb72c28f3c24506d1c735ab85
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-04-22 11:14:58 +0700

    update

--- application/modules/pembelian/views/penagihan_pembelian_detail.php
+++ application/modules/pembelian/views/penagihan_pembelian_detail.php
@@ -329,7 +329,7 @@
                                 <br />
                                 <?php if ($row["verifikasi_dok"] === "SUDAH") : ?>
                                     <?php
-                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 2 || $this->session->userdata('admin_group_id') == 3 || $this->session->userdata('admin_group_id') == 4 || $this->session->userdata('admin_group_id') == 6){
+                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 16){
                                     ?>
                                     <a href="<?= site_url('pembelian/pembayaran_panagihan/' . $row['id']); ?>" class="btn btn-default" style="width:150px; font-weight:bold; border-radius:10px;"> KIRIM PEMBAYARAN</a>
                                     <a href="<?= site_url('pembelian/closed_pembayaran_penagihan/' . $row['id']); ?>" class="btn btn-default" style="width:150px; font-weight:bold; border-radius:10px;"> PEMBAYARAN LUNAS</a>
@@ -338,7 +338,7 @@
                                     ?>
 
                                     <?php
-                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 2 || $this->session->userdata('admin_group_id') == 3 || $this->session->userdata('admin_group_id') == 4){
+                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 16){
                                     ?>
                                     <a href="<?= base_url('pembelian/sunting_tagihan/' . $row["id"]) ?>" class="btn btn-default" style="width:150px; font-weight:bold; border-radius:10px;"> EDIT TAGIHAN</a>
                                     <a href="<?= base_url('pembelian/sunting_verifikasi/' . $row["id"]) ?>" class="btn btn-default" style="width:150px; font-weight:bold; border-radius:10px;"> EDIT VERIFIKASI</a>
@@ -351,7 +351,7 @@
 
                                 <?php if ($row["verifikasi_dok"] === "LENGKAP") : ?>
                                     <?php
-                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 2 || $this->session->userdata('admin_group_id') == 3 || $this->session->userdata('admin_group_id') == 4){
+                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 16){
                                     ?>
                                     <a href="<?= site_url('pembelian/open_penagihan/' . $row['id']); ?>" class="btn btn-default" style="width:20%; font-weight:bold; border-radius:10px;"> PEMBAYARAN BELUM LUNAS</a>
                                     <?php
@@ -359,7 +359,7 @@
                                     ?>
 
                                     <?php
-                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 2 || $this->session->userdata('admin_group_id') == 3 || $this->session->userdata('admin_group_id') == 4 || $this->session->userdata('admin_group_id') == 6){
+                                    if($this->session->userdata('admin_group_id') == 1 || $this->session->userdata('admin_group_id') == 5 || $this->session->userdata('admin_group_id') == 6 || $this->session->userdata('admin_group_id') == 16){
                                     ?>
                                     <a class="btn btn-default" onclick="DeleteData('<?= site_url('pembelian/delete_penagihan_pembelian/' . $row['id']); ?>')" style="width:150px; font-weight:bold; border-radius:10px;"> HAPUS</a>
                                     <?php
