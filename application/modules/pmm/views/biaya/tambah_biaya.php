cd12bf8e4fd7f03a7e4433cb0a5b145d2dc89f40
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-05-03 13:34:11 +0700

    update

--- application/modules/pmm/views/biaya/tambah_biaya.php
+++ application/modules/pmm/views/biaya/tambah_biaya.php
@@ -250,8 +250,8 @@
             locale: {
               format: 'DD-MM-YYYY'
             },
-            minDate: new Date()+0,
-			maxDate: new Date()+1,
+            //minDate: new Date()+0,
+			//maxDate: new Date()+1,
             //minDate: moment().add(-10, 'd').toDate(),
 			//maxDate: moment().add(+0, 'd').toDate(),
             //minDate: moment().add(-1, 'month').toDate(),
