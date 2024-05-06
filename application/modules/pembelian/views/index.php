1c7865e74f45a36ab5022bcfa8be053225e0675f
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-05-06 14:02:22 +0700

    update

--- application/modules/pembelian/views/index.php
+++ application/modules/pembelian/views/index.php
@@ -104,9 +104,9 @@
                                     ?>
                                         <li role="presentation"><a href="#verifikasi" aria-controls="verifikasi" role="tab" data-toggle="tab" style="border-radius:10px 0px 10px 0px; font-weight:bold;">NOTIFIKASI 
                                             <blink><b><?php
-                                            $query = $this->db->query('SELECT * FROM pmm_verifikasi_penagihan_pembelian where approve_unit_head = "TIDAK DISETUJUI" ');
-                                            echo $query->num_rows();
-                                            $query = $this->db->query('SELECT * FROM pmm_purchase_order where status = "WAITING" ');
+                                            $query = 
+                                            $this->db->query('SELECT * FROM pmm_verifikasi_penagihan_pembelian where approve_unit_head = "TIDAK DISETUJUI" ');
+                                            $this->db->query('SELECT * FROM pmm_purchase_order where status = "WAITING" ');
                                             echo $query->num_rows();
                                             ?></b></blink>
                                         </a></li>			
