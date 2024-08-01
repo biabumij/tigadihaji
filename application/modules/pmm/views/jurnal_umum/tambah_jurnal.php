ffb499168129b1790381a62968efa4bf3c8a6bb6
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-08-01 12:59:51 +0700

    update

--- application/modules/pmm/views/jurnal_umum/tambah_jurnal.php
+++ application/modules/pmm/views/jurnal_umum/tambah_jurnal.php
@@ -161,10 +161,10 @@
             },
             //minDate: new Date()+0,
 			//maxDate: new Date()+1,
-            minDate: moment().add(-26, 'd').toDate(),
+            //minDate: moment().add(-26, 'd').toDate(),
 			//maxDate: moment().add(+0, 'd').toDate(),
             //minDate: moment().add(-5, 'month').toDate(),
-            //minDate: moment().startOf('month').toDate(),
+            minDate: moment().startOf('month').toDate(),
 			maxDate: moment().endOf('month').toDate(),	
         });
         
