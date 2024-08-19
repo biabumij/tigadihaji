27ab2004bf11d32570418c375ffc99e1fba1ed24
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-08-19 12:14:35 +0700

    update

--- application/modules/pmm/views/receipt_material_print.php
+++ application/modules/pmm/views/receipt_material_print.php
@@ -63,9 +63,9 @@
                 <th align="center" width="3%">No</th>
                 <th align="center" width="7%">Tanggal</th>
 				        <th align="left" width="22%">No. Pesanan Pembelian</th>
-                <th align="center" width="10%">No. Surat Jalan</th>
-                <th align="center" width="10%">No. Kendaraan</th>
-                <th align="center" width="10%">Supir</th>
+                <th align="left" width="10%">No. Surat Jalan</th>
+                <th align="left" width="10%">No. Kendaraan</th>
+                <th align="left" width="10%">Supir</th>
                 <th align="center" width="10%">Produk</th>
                 <th align="center" width="6%">Satuan</th>
                 <th align="center" width="6%">Volume</th>
@@ -105,9 +105,9 @@
                   <td><?php echo $key + 1 ;?></td>
                   <td><?php echo date('d-m-Y',strtotime($row['date_receipt']));?></td>
                   <td style="text-align:left;"><?php echo $row['no_po'];?></td>
-                  <td><?php echo $row['surat_jalan'];?></td>
-                  <td><?php echo $row['no_kendaraan'];?></td>
-                  <td><?php echo $row['driver'];?></td>
+                  <td style="text-align:left;"><?php echo $row['surat_jalan'];?></td>
+                  <td style="text-align:left;"><?php echo $row['no_kendaraan'];?></td>
+                  <td style="text-align:left;"><?php echo $row['driver'];?></td>
                   <td><?php echo $this->crud_global->GetField('produk',array('id'=>$row['material_id']),'nama_produk');?></td>
                   <td><?php echo $row['measure'];?></td>
                   <td><?php echo number_format($row['volume'],2,',','.');?></td>
