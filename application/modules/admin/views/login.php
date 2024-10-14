d3374e9cb86d1451e02d05e5ac742ebfbd5e7d08
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-10-14 14:30:15 +0700

    update

--- application/modules/admin/views/login.php
+++ application/modules/admin/views/login.php
@@ -7,7 +7,6 @@
 	    <meta charset="UTF-8">
 		<meta name="description" key="description" content="Sistem Produksi Beton">
 		<meta name="title" key="title" content="TIGA DIHAJI">
-		<meta name="author" content="Ginanjar Bayu Bimantoro">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 	    <title><?php echo $this->m_themes->GetThemes('site_name');?> - LOGIN</title>
 	    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url().$this->m_themes->GetThemes('site_favico');?>">
