6e3e99074f022221ceb740464cebc610d5d9a887
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-10-14 12:34:42 +0700

    update

--- application/modules/penjualan/controllers/Penjualan.php
+++ application/modules/penjualan/controllers/Penjualan.php
@@ -386,7 +386,7 @@ class Penjualan extends Secure_Controller
 		$this->db->select('ps.*, p.nama as client_name');
 		$this->db->join('penerima p', 'ps.client_id = p.id', 'left');
 		$this->db->where("ps.status <> 'REJECT'");
-		$this->db->order_by('ps.status','WAITING','DESC');
+		$this->db->order_by('ps.status','DESC');
 		$this->db->order_by('ps.created_on', 'DESC');
 		$query = $this->db->get('pmm_sales_po ps');
 		if ($query->num_rows() > 0) {
