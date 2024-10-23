0f57c8b6331ca1badec8d8a7cfca1da6de224da6
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-10-23 13:08:48 +0700

    update

--- application/modules/admin/views/dashboard.php
+++ application/modules/admin/views/dashboard.php
@@ -633,7 +633,7 @@
                                 format: '{value} M3'
                             },
                             min: 0,
-                            max: 3500,
+                            max: 4000,
                             tickInterval:500,
                         },
                         tooltip: {
