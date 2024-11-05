1085072f5c2ce3b5c215aaf591073d673bfd39b4
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-11-05 11:53:04 +0700

    update

--- application/modules/pmm/controllers/Reports.php
+++ application/modules/pmm/controllers/Reports.php
@@ -15778,7 +15778,7 @@ class Reports extends CI_Controller {
 			$rap_bua = $this->db->select('sum(det.jumlah) as total')
 			->from('rap_bua rap')
 			->join('rap_bua_detail det','rap.id = det.rap_bua_id','left')
-			->where("det.akun <> 131 ")
+			->where("det.coa <> 131 ")
 			->where("rap.status = 'PUBLISH'")
 			->where("rap.tanggal_rap_bua < '$date2'")
 			->order_by('rap.tanggal_rap_bua','asc')->limit(1)
