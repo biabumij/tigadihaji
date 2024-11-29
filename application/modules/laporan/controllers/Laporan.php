fbcf4d46e69c33573743c3f1c3f197fb232b5170
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-11-29 15:58:04 +0700

    update

--- application/modules/laporan/controllers/Laporan.php
+++ application/modules/laporan/controllers/Laporan.php
@@ -2480,6 +2480,37 @@ class Laporan extends Secure_Controller {
 	
 	}
 
+	public function laporan_evaluasi_biaya_produksi_pemakaian_print()
+	{
+		$this->load->library('pdf');
+	
+		$pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
+        $pdf->setPrintHeader(true);
+		$pdf->setPrintFooter(true);
+        $tagvs = array('div' => array(0 => array('h' => 0, 'n' => 0), 1 => array('h' => 0, 'n'=> 0)));
+		$pdf->setHtmlVSpace($tagvs);
+		$pdf->AddPage('P');
+
+		$arr_date = $this->input->get('filter_date');
+		if(empty($arr_date)){
+			$filter_date = '-';
+		}else {
+			$arr_filter_date = explode(' - ', $arr_date);
+			$start_date = date('Y-m-d',strtotime($arr_filter_date[0]));
+			$end_date = date('Y-m-d',strtotime($arr_filter_date[1]));
+			$filter_date = date('d F Y',strtotime($arr_filter_date[0])).' - '.date('d F Y',strtotime($arr_filter_date[1]));
+		}
+		$data['filter_date'] = $filter_date;
+		$data['start_date'] = $start_date;
+		$data['end_date'] = $end_date;
+        $html = $this->load->view('laporan_produksi/laporan_evaluasi_biaya_produksi_pemakaian_print',$data,TRUE);
+
+        $pdf->SetTitle('BBJ - Laporan Evaluasi Biaya Produksi');
+        $pdf->nsi_html($html);
+        $pdf->Output('laporan-evaluasi-biaya-produksi.pdf', 'I');
+	
+	}
+
 	public function cetak_detail_bp()
 	{
 		$this->load->library('pdf');
