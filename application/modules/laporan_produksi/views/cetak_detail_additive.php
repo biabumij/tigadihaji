7d93efd1ba14e8e8b2cfe76db1b441ecd161db9b
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-12-03 16:41:28 +0700

    update

--- application/modules/laporan_produksi/views/cetak_detail_additive.php
+++ application/modules/laporan_produksi/views/cetak_detail_additive.php
@@ -116,43 +116,56 @@
 			->where("cat.material_id = 19")
 			->where("cat.status = 'PUBLISH'")
 			->order_by('date','desc')->limit(1)
-			->get()->row_array();
+			->get()->result_array();
 
-			$stok_volume_lalu = $stock_opname_ago['volume'];
-			$stok_nilai_lalu = $stock_opname_ago['nilai'];
+			$stock_volume_lalu = 0;
+			$stock_nilai_lalu = 0;
+			foreach ($stock_opname_ago as $x){
+				$stock_volume_lalu += $x['volume'];
+				$stock_nilai_lalu += $x['nilai'];
+			}
 
-			$pembelian = $this->db->select('SUM(prm.display_volume) as volume, SUM(prm.display_price) as nilai')
+			$pembelian = $this->db->select('ps.nama as nama, SUM(prm.display_volume) as volume, SUM(prm.display_price) as nilai')
 			->from('pmm_receipt_material prm')
 			->join('pmm_purchase_order po', 'prm.purchase_order_id = po.id','left')
 			->join('produk p', 'prm.material_id = p.id','left')
+			->join('penerima ps', 'po.supplier_id = ps.id','left')
 			->where("(prm.date_receipt between '$date1' and '$date2')")
 			->where("p.kategori_bahan = '6'")
 			->where("po.status in ('PUBLISH','CLOSED')")
-			->get()->row_array();
+			->group_by("po.supplier_id")
+			->order_by('po.date_po','asc')
+			->get()->result_array();
 
-			$pembelian_volume = $pembelian['volume'];
-			$pembelian_nilai = $pembelian['nilai'];
+			$pembelian_volume = 0;
+			$pembelian_nilai = 0;
+			foreach ($pembelian as $x){
+				$pembelian_volume += $x['volume'];
+				$pembelian_nilai += $x['nilai'];
+			}
 			
 			$pemakaian = $this->db->select('sum(volume) as volume, sum(nilai) as nilai')
 			->from('pemakaian_bahan')
 			->where("date between '$date1' and '$date2'")
 			->where("material_id = 19")
 			->where("status = 'PUBLISH'")
-			->group_by("id")
-			->get()->row_array();
-
-			$pemakaian_volume = $pemakaian['volume'];
-			$pemakaian_nilai = $pemakaian['nilai'];
-
-			$stock_volume = ($stok_volume_lalu + $pembelian_volume) - $pemakaian_volume;
-			$stock_nilai = ($stok_nilai_lalu + $pembelian_nilai) - $pemakaian_nilai;
+			->get()->result_array();
+			
+			$pemakaian_volume = 0;
+			$pemakaian_nilai = 0;
+			foreach ($pemakaian as $x){
+				$pemakaian_volume += $x['volume'];
+				$pemakaian_nilai += $x['nilai'];
+			}
+			
 			?>
 		<table cellpadding="3" width="98%">
 			<tr class="table-judul">
-				<th align="center" colspan="2">PERSEDIAAN AWAL</th>
-				<th align="center" colspan="2">PEMBELIAN</th>
-				<th align="center" colspan="2">PEMAKAIAN</th>
-				<th align="center" colspan="2">STOCK OPNAME</th>
+				<th align="center" colspan="2" width="20%">PERSEDIAAN AWAL</th>
+				<th align="center" rowspan="2" width="20%">&nbsp; <br />REKANAN</th>
+				<th align="center" colspan="2" width="20%">PEMBELIAN</th>
+				<th align="center" colspan="2" width="20%">PEMAKAIAN</th>
+				<th align="center" colspan="2" width="20%">STOCK OPNAME</th>
 			</tr>
 			<tr class="table-judul">
 				<th align="right">VOLUME</th>
@@ -164,17 +177,30 @@
 				<th align="right">VOLUME</th>
 				<th align="right">NILAI</th>
 			</tr>
+			<?php foreach ($pembelian as $i=>$x): ?>
 			<tr class="table-baris1">
-				<td align="right"><?php echo number_format($stok_volume_lalu,2,',','.');?></td>
-				<td align="right"><?php echo number_format($stok_nilai_lalu,0,',','.');?></td>
+				<td align="right"><?php echo number_format($stock_volume_lalu[$i]['volume'],2,',','.');?></td>
+				<td align="right"><?php echo number_format($stock_nilai_lalu[$i]['nilai'],0,',','.');?></td>
+				<td align="right"><?= $pembelian[$i]['nama'] ?></td>
+				<td align="right"><?php echo number_format($pembelian[$i]['volume'],2,',','.');?></td>
+				<td align="right"><?php echo number_format($pembelian[$i]['nilai'],0,',','.');?></td>
+				<td align="right"><?php echo number_format($pemakaian[$i]['volume'],2,',','.');?></td>
+				<td align="right"><?php echo number_format($pemakaian[$i]['nilai'],0,',','.');?></td>
+				<td align="right"><?php echo number_format($stock_volume_lalu + $pembelian[$i]['volume'] - $pemakaian[$i]['volume'],2,',','.');?></td>
+				<td align="right"><?php echo number_format($stock_nilai_lalu + $pembelian[$i]['nilai'] - $pemakaian[$i]['nilai'],0,',','.');?></td>
+			</tr>
+			<?php endforeach; ?>
+			<tr class="table-total">
+				<td align="right"><?php echo number_format($stock_volume_lalu,2,',','.');?></td>
+				<td align="right"><?php echo number_format($stock_nilai_lalu,0,',','.');?></td>
+				<td align="right"></td>
 				<td align="right"><?php echo number_format($pembelian_volume,2,',','.');?></td>
 				<td align="right"><?php echo number_format($pembelian_nilai,0,',','.');?></td>
 				<td align="right"><?php echo number_format($pemakaian_volume,2,',','.');?></td>
 				<td align="right"><?php echo number_format($pemakaian_nilai,0,',','.');?></td>
-				<td align="right"><?php echo number_format($stock_volume,2,',','.');?></td>
-				<td align="right"><?php echo number_format($stock_nilai,0,',','.');?></td>
+				<td align="right"><?php echo number_format($stock_volume_lalu + $pembelian_volume - $pemakaian_volume,2,',','.');?></td>
+				<td align="right"><?php echo number_format($stock_nilai_lalu + $pembelian_nilai - $pemakaian_nilai,0,',','.');?></td>
 			</tr>
-			
 		</table>
 	</body>
 </html>
