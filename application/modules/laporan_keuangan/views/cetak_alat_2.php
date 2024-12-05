eab21739652233deac404987c07fce4b07f94599
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-12-05 13:48:51 +0700

    update

--- application/modules/laporan_keuangan/views/cetak_alat_2.php
+++ application/modules/laporan_keuangan/views/cetak_alat_2.php
@@ -3,6 +3,43 @@
 	<head>
 	  <title>BIAYA ALAT</title>
 	  
+	  <?php
+		$search = array(
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+		);
+		
+		$replace = array(
+		'Januari',
+		'Februari',
+		'Maret',
+		'April',
+		'Mei',
+		'Juni',
+		'Juli',
+		'Agustus',
+		'September',
+		'Oktober',
+		'November',
+		'Desember'
+		);
+		
+		$subject = "$filter_date";
+
+		echo str_replace($search, $replace, $subject);
+
+	  ?>
+	  
 	  <style type="text/css">
 		body {
 			font-family: helvetica;
@@ -28,37 +65,7 @@
 
 	</head>
 	<body>
-		<div align="center" style="display: block;font-weight:bold; font-size: 11px;">Biaya Alat</div>
-		<div align="center" style="display: block;font-weight:bold; font-size: 11px;">Proyek Bendungan Tiga Dihaji</div>
-		<?php
-		function tgl_indo($date2){
-			$bulan = array (
-				1 =>   'Januari',
-				'Februari',
-				'Maret',
-				'April',
-				'Mei',
-				'Juni',
-				'Juli',
-				'Agustus',
-				'September',
-				'Oktober',
-				'November',
-				'Desember'
-			);
-			$pecahkan = explode('-', $date2);
-			
-			// variabel pecahkan 0 = tanggal
-			// variabel pecahkan 1 = bulan
-			// variabel pecahkan 2 = tahun
-		
-			return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
-			
-		}
-		?>
-		<div style="display: block;font-weight: bold;font-size: 11px;">Periode SD. <?= tgl_indo(date($date2)); ?></div>
-		<br /><br /><br />
-		<?php
+	<?php
 		$data = array();
 		
 		$arr_date = $this->input->get('filter_date');
@@ -73,9 +80,47 @@
 			$date2 	= date('Y-m-d',strtotime($arr_filter_date[1]));
 			$filter_date = date('d F Y',strtotime($arr_filter_date[0])).' - '.date('d F Y',strtotime($arr_filter_date[1]));
 		}
-		
 		?>
-		
+		<br />
+		<br />
+		<table width="98%" cellpadding="3">
+			<tr>
+				<td align="center"  width="100%">
+					<div style="display: block;font-weight: bold;font-size: 11px;">Biaya Alat</div>
+					<div style="display: block;font-weight: bold;font-size: 11px;">Proyek Bendungan Tiga Dihaji</div>
+					<?php
+					function tgl_indo($date2){
+						$bulan = array (
+							1 =>   'Januari',
+							'Februari',
+							'Maret',
+							'April',
+							'Mei',
+							'Juni',
+							'Juli',
+							'Agustus',
+							'September',
+							'Oktober',
+							'November',
+							'Desember'
+						);
+						$pecahkan = explode('-', $date2);
+						
+						// variabel pecahkan 0 = tanggal
+						// variabel pecahkan 1 = bulan
+						// variabel pecahkan 2 = tahun
+					
+						return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+						
+					}
+					?>
+					<div style="display: block;font-weight: bold;font-size: 11px;">Periode SD. <?= tgl_indo(date($date2)); ?></div>
+				</td>
+			</tr>
+		</table>
+		<br />
+		<br />
+		<br />
 		<table width="98%" border="0" cellpadding="3" border="0">
 		
 			<?php
