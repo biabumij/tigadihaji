e9550a57514aaeb14edd3f19cb2f3dc783efa27f
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-01-10 10:53:11 +0700

    update

--- application/modules/laporan_produksi/views/cetak_detail_tm.php
+++ application/modules/laporan_produksi/views/cetak_detail_tm.php
@@ -549,23 +549,23 @@
 				?>
 				<th align="right" style="<?php echo $styleColor ?>; border-right:1px solid black;"><?php echo $total_nilai_evaluasi_truck_mixer < 0 ? "(".number_format(-$total_nilai_evaluasi_truck_mixer,0,',','.').")" : number_format($total_nilai_evaluasi_truck_mixer,0,',','.');?></th>
 	        </tr>
-			<tr class="table-baris1">		
-				<th align="left" style="border-left:1px solid black;">&nbsp;&nbsp;Pemeliharaan</th>
-				<th align="center" style="border-right:1px solid black;">M3</th>
-				<th align="right"><?php echo number_format($vol_pemeliharaan_truck_mixer,2,',','.');?></th>
-				<th align="right"><?php echo number_format($harsat_pemeliharaan_truck_mixer,0,',','.');?></th>
-				<th align="right" style="border-right:1px solid black;"><?php echo number_format($pemeliharaan_truck_mixer,0,',','.');?></th>
-				<th align="right"><?php echo number_format(0,2,',','.');?></th>
-				<th align="right"><?php echo number_format(0,0,',','.');?></th>
-				<th align="right" style="border-right:1px solid black;"><?php echo number_format($total_pemakaian_pemeliharaan_truck_mixer,0,',','.');?></th>
+			<tr class="table-baris1">			
+				<th align="left" style="border-left:1px solid black; border-bottom:1px solid black;">&nbsp;&nbsp;Pemeliharaan</th>
+				<th align="center" style="border-right:1px solid black; border-bottom:1px solid black;">M3</th>
+				<th align="right" style="border-bottom:1px solid black;"><?php echo number_format($vol_pemeliharaan_truck_mixer,2,',','.');?></th>
+				<th align="right" style="border-bottom:1px solid black;"><?php echo number_format($harsat_pemeliharaan_truck_mixer,0,',','.');?></th>
+				<th align="right" style="border-right:1px solid black; border-bottom:1px solid black;"><?php echo number_format($pemeliharaan_truck_mixer,0,',','.');?></th>
+				<th align="right" style="border-bottom:1px solid black;"><?php echo number_format(0,2,',','.');?></th>
+				<th align="right" style="border-bottom:1px solid black;"><?php echo number_format(0,0,',','.');?></th>
+				<th align="right" style="border-right:1px solid black; border-bottom:1px solid black;"><?php echo number_format($total_pemakaian_pemeliharaan_truck_mixer,0,',','.');?></th>
 				<?php
 				$styleColor = $total_vol_evaluasi_pemeliharaan_truck_mixer < 0 ? 'color:red' : 'color:black';
 				?>
-				<th align="right" style="<?php echo $styleColor ?>"><?php echo $total_vol_evaluasi_pemeliharaan_truck_mixer < 0 ? "(".number_format(-$total_vol_evaluasi_pemeliharaan_truck_mixer,2,',','.').")" : number_format($total_vol_evaluasi_pemeliharaan_truck_mixer,2,',','.');?></th>
+				<th align="right" style="<?php echo $styleColor ?>; border-bottom:1px solid black;"><?php echo $total_vol_evaluasi_pemeliharaan_truck_mixer < 0 ? "(".number_format(-$total_vol_evaluasi_pemeliharaan_truck_mixer,2,',','.').")" : number_format($total_vol_evaluasi_pemeliharaan_truck_mixer,2,',','.');?></th>
 				<?php
 				$styleColor = $total_nilai_evaluasi_pemeliharaan_truck_mixer < 0 ? 'color:red' : 'color:black';
 				?>
-				<th align="right" style="<?php echo $styleColor ?>; border-right:1px solid black;"><?php echo $total_nilai_evaluasi_pemeliharaan_truck_mixer < 0 ? "(".number_format(-$total_nilai_evaluasi_pemeliharaan_truck_mixer,0,',','.').")" : number_format($total_nilai_evaluasi_pemeliharaan_truck_mixer,0,',','.');?></th>
+				<th align="right" style="<?php echo $styleColor ?>; border-right:1px solid black; border-bottom:1px solid black;"><?php echo $total_nilai_evaluasi_pemeliharaan_truck_mixer < 0 ? "(".number_format(-$total_nilai_evaluasi_pemeliharaan_truck_mixer,0,',','.').")" : number_format($total_nilai_evaluasi_pemeliharaan_truck_mixer,0,',','.');?></th>
 	        </tr>
 	    </table>
 		<br /><br /><br /><br /><br /><br /><br /><br /><br />
