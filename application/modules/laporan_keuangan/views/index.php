9a8496ff41b6d3af4c47359993cc3340b3cdeb36
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-04-17 09:57:29 +0700

    update

--- application/modules/laporan_keuangan/views/index.php
+++ application/modules/laporan_keuangan/views/index.php
@@ -79,7 +79,7 @@
                                                     <a href="#laporan-laba-rugi" aria-controls="laporan-laba-rugi" role="tab" data-toggle="tab" class="btn btn-primary" style="border-radius:10px; font-weight:bold;">Lihat Laporan</a>										
                                                 </div>
                                                 <?php
-                                                if(in_array($this->session->userdata('admin_group_id'), array(1,3,8))){
+                                                if(in_array($this->session->userdata('admin_id'), array(1,2,11))){
                                                 ?>
                                                 <div class="col-sm-5">
                                                     <p><h5><b>Neraca</b></h5></p>
