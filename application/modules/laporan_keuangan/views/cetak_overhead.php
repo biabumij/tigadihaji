9343c626144a286a8296d2ed778c3f90dae05d3f
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-05-08 10:41:43 +0700

    update

--- application/modules/laporan_keuangan/views/cetak_overhead.php
+++ application/modules/laporan_keuangan/views/cetak_overhead.php
@@ -549,10 +549,10 @@
 						</tr>
 						<tr>
 							<td align="center" height="55px">
-								
+								<img src="uploads/ttd_novel.png" width="50px">
 							</td>
 							<td align="center">
-								
+								<img src="uploads/ttd_avriani.png" width="50px">
 							</td>
 						</tr>
 						<tr>
