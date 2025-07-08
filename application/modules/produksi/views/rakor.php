929b30d06db2a5e59d740aa3cb6d01afaeb169c4
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-07-08 12:51:58 +0700

    update

--- application/modules/produksi/views/rakor.php
+++ application/modules/produksi/views/rakor.php
@@ -9,14 +9,12 @@
 <?php
     if(in_array($this->session->userdata('admin_group_id'), array(1,2,3,4,5))){
     ?>
-    <div class="col-sm-2">
+    <div class="col-sm-3">
     <button style="background-color:#88b93c; border:1px solid black; border-radius:10px; line-height:30px;"><a href="<?php echo site_url('produksi/form_rakor'); ?>"><b style="color:white;">BUAT KUNCI</b></a></button>
     </div>
     <?php
     }
     ?>
-<br />
-<br />
 <br />
 <br />
 <div class="table-responsive">
