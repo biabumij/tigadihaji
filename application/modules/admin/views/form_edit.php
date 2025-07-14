aa714e0c76679e6170d86cd798d5a730db508d1c
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-07-14 14:44:48 +0700

    update

--- application/modules/admin/views/form_edit.php
+++ application/modules/admin/views/form_edit.php
@@ -161,7 +161,7 @@
                                         <br />
                                         <br />
                                         <a href="<?php echo site_url('admin/admin'); ?>" class="btn btn-danger" style="width:5%; font-weight:bold; border-radius:10px; margin-top: 10px;"> KEMBALI</a>
-                                        <button type="submit" name="submit" class="btn btn-success" id="btn-submit" data-loading-text="please wait.." style="font-weight:bold; border-radius:10px;">KIRIM</button>
+                                        <button type="submit" name="submit" class="btn btn-success" id="btn-submit" data-loading-text="please wait.." style="width:5%; font-weight:bold; border-radius:10px;">KIRIM</button>
                                         <br />
                                         <br />
                                         <br />
