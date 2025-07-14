9724e9accdb2a41b692cbe2453ec9831246177df
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-07-14 15:16:56 +0700

    update

--- application/modules/pembelian/views/penawaran_pembelian_detail.php
+++ application/modules/pembelian/views/penawaran_pembelian_detail.php
@@ -214,8 +214,8 @@
                                         <?php
                                         if($approval_penawaran_pembelian == 1){
                                         ?>
-                                            <a href="<?= site_url('pembelian/approve_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-success" style="width:15%; font-weight:bold; border-radius:10px;"> SETUJUI</a>
-                                            <a href="<?= site_url('pembelian/reject_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:15%; font-weight:bold; border-radius:10px;"> TOLAK</a>
+                                            <a href="<?= site_url('pembelian/approve_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-success" style="width:10%; font-weight:bold; border-radius:10px;"> SETUJUI</a>
+                                            <a href="<?= site_url('pembelian/reject_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:10%; font-weight:bold; border-radius:10px;"> TOLAK</a>
                                         <?php
                                         }
                                         ?>
@@ -225,8 +225,8 @@
                                         <?php
                                         if($approval_penawaran_pembelian == 1){
                                         ?>
-                                            <a href="<?= site_url('pembelian/closed_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-default" style="width:15%; font-weight:bold; border-radius:10px;"> CLOSED</a>
-                                            <a href="<?= site_url('pembelian/reject_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:15%; font-weight:bold; border-radius:10px;"> REJECT</a>
+                                            <a href="<?= site_url('pembelian/closed_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-default" style="width:10%; font-weight:bold; border-radius:10px;"> CLOSED</a>
+                                            <a href="<?= site_url('pembelian/reject_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:10%; font-weight:bold; border-radius:10px;"> REJECT</a>
                                         <?php
                                         }
                                         ?>
@@ -236,14 +236,14 @@
                                         <?php
                                         if($approval_penawaran_pembelian == 1){
                                         ?>
-                                            <a href="<?= site_url('pembelian/open_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-default" style="width:15%; font-weight:bold; border-radius:10px;"> OPEN</a>
+                                            <a href="<?= site_url('pembelian/open_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-default" style="width:10%; font-weight:bold; border-radius:10px;"> OPEN</a>
                                         <?php
                                         }
                                         ?>
                                         <?php
                                         if($delete_penawaran_pembelian == 1){
                                         ?>
-                                            <a href="<?= site_url('pembelian/hapus_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:15%; font-weight:bold; border-radius:10px;"> HAPUS</a>
+                                            <a href="<?= site_url('pembelian/hapus_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:10%; font-weight:bold; border-radius:10px;"> HAPUS</a>
                                         <?php
                                         }
                                         ?>
@@ -253,7 +253,7 @@
                                         <?php
                                         if($delete_penawaran_pembelian == 1){
                                         ?>
-                                            <a href="<?= site_url('pembelian/hapus_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:15%; font-weight:bold; border-radius:10px;"> HAPUS</a>
+                                            <a href="<?= site_url('pembelian/hapus_penawaran_pembelian/' . $row['id']); ?>" class="btn btn-danger" style="width:10%; font-weight:bold; border-radius:10px;"> HAPUS</a>
                                         <?php
                                         }
                                         ?>
