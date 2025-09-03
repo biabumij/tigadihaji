be72a87b4df4960cfe6041a74abdfcba633e2aaa
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-09-03 16:08:50 +0700

    update

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -77,6 +77,6 @@ class Pdf extends TCPDF {
         $this->SetTextColor(128,128,128);
         $this->SetFont('helvetica', 'I', 6);
         $this->SetMargins(23, 23, 23, true);
-        $this->Cell(0, 10, 'Dicetak pada :'.'.'.$date.'', 0, 0,'R');
+        $this->Cell(0, 10, 'Dicetak pada :'.''.$date.'', 0, 0,'R');
     }
 } 
