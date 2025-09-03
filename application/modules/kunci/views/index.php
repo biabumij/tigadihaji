c621563820ee831722b7a548b9828947ebc0abde
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-09-03 11:06:18 +0700

    update

---
+++ application/modules/kunci/views/index.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en" class="fixed">
+    <head>
+        <?php echo $this->Templates->Header(); ?>
+        <style type="text/css">
+            body {
+                font-family: helvetica;
+            }
+            
+            .mytable thead th {
+            background-color:	#e69500;
+            color: #ffffff;
+            text-align: center;
+            vertical-align: middle;
+            padding: 5px;
+            }
+            
+            .mytable tbody td {
+            padding: 5px;
+            }
+            
+            .mytable tfoot td {
+            background-color:	#e69500;
+            color: #FFFFFF;
+            padding: 5px;
+            }
+
+            button {
+                border: none;
+                border-radius: 5px;
+                padding: 5px;
+                font-size: 12px;
+                text-transform: uppercase;
+                cursor: pointer;
+                color: white;
+                background-color: #2196f3;
+                box-shadow: 0 0 4px #999;
+                outline: none;
+            }
+
+            .ripple {
+                background-position: center;
+                transition: background 0.8s;
+            }
+            .ripple:hover {
+                background: #47a7f5 radial-gradient(circle, transparent 1%, #47a7f5 1%) center/15000%;
+            }
+            .ripple:active {
+                background-color: #6eb9f7;
+                background-size: 100%;
+                transition: background 0s;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="wrap">
+            <?php echo $this->Templates->PageHeader(); ?>
+            <div class="page-body">
+                <div class="content">
+                    <div class="row animated fadeInUp">
+                        <div class="col-sm-12 col-lg-12">
+                            <div class="panel">
+                                <div class="panel-header">
+                                    <h3><b style="text-transform:uppercase;"><?php echo $row[0]->menu_name; ?></b></h3>
+                                    <div class="text-left">
+                                        <a href="<?php echo site_url('admin');?>">
+                                        <button class="ripple"><b><i class="fa-solid fa-rotate-left"></i> KEMBALI</b></button></a>
+                                    </div>
+                                </div>
+                                <div class="panel-content">
+                                    <ul class="nav nav-tabs" role="tablist" style="border-bottom:2px solid #e69500; padding-bottom:10px;">
+                                        <?php
+                                        $admin_id = $this->session->userdata('admin_id');
+                                        $approval = $this->db->select('*')
+                                        ->from('tbl_admin')
+                                        ->where("admin_id = $admin_id ")
+                                        ->get()->row_array();
+                                        $menu_admin =  $approval['menu_admin'];
+                                        $kunci_rakor =  $approval['kunci_rakor'];
+                                        ?>
+                                        <?php if($menu_admin == 1){?>
+                                        <li role="presentation"  class="active"><a href="#pemakaian" aria-controls="pemakaian" role="tab" data-toggle="tab" style="border-radius:5px; font-weight:bold;">VERIFIKASI PEMAKAIAN BAHAN</a></li>
+                                        <?php
+                                        }
+                                        ?>
+                                        <?php if($kunci_rakor == 1){?>
+                                        <li role="presentation"><a href="#rakor" aria-controls="rakor" role="tab" data-toggle="tab" style="border-radius:5px; font-weight:bold;">KUNCI DATA RAKOR</a></li>
+                                        <?php
+                                        }
+                                        ?>
+                                    </ul>
+                                    <div class="tab-content">
+                                        <br />
+                                        <div role="tabpanel" class="tab-pane active" id="pemakaian">
+                                            <?php include_once "pemakaian.php"; ?>
+                                        </div>
+                                        <div role="tabpanel" class="tab-pane" id="rakor">
+                                            <?php include_once "rakor.php"; ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php echo $this->Templates->Footer(); ?>
+        <script src="<?php echo base_url(); ?>assets/back/theme/vendor/jquery.number.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/back/theme/vendor/bootbox.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/select/1.3.1/css/select.dataTables.min.css">
+        <script type="text/javascript" src="https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/back/theme/vendor/daterangepicker/moment.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/back/theme/vendor/daterangepicker/daterangepicker.js"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/back/theme/vendor/daterangepicker/daterangepicker.css">
+        <script src="https://kit.fontawesome.com/591a1bf2f6.js" crossorigin="anonymous"></script>
+        <?php include_once("script_pemakaian.php"); ?>
+        <?php include_once("script_rakor.php"); ?>
+
+        <script>
+            <?php
+            $kunci_rakor = $this->db->select('date')->order_by('date','desc')->limit(1)->get_where('kunci_rakor')->row_array();
+            $last_opname = date('d-m-Y', strtotime('+1 days', strtotime($kunci_rakor['date'])));
+            ?>
+            $('.dtpicker').daterangepicker({
+                singleDatePicker: true,
+                showDropdowns : false,
+                locale: {
+                    format: 'DD-MM-YYYY'
+                },
+                minDate: '<?php echo $last_opname;?>',
+                //maxDate: moment().add(+0, 'd').toDate(),
+                //minDate: moment().startOf('month').toDate(),
+                maxDate: moment().endOf('month').toDate(),
+            });
+        </script>
+    </body>
+</html>
---
+++ application/modules/kunci/views/pemakaian.php
@@ -0,0 +1,41 @@
+<form action="<?php echo site_url('kunci/cetak_kunci'); ?>" target="_blank">
+    <div class="col-sm-3">
+        <input type="text" id="filter_date_pemakaian" name="filter_date" class="form-control dtpickerangepemakaian" autocomplete="off" placeholder="Filter By Date">
+    </div>
+    <!--<div class="col-sm-1">
+        <button type="submit" class="btn btn-default" style="border-radius:5px; font-weight:bold;">PRINT</button>
+    </div>-->
+</form>
+<?php
+$admin_id = $this->session->userdata('admin_id');
+$approval = $this->db->select('*')
+->from('tbl_admin')
+->where("admin_id = $admin_id ")
+->get()->row_array();
+$kunci_rakor =  $approval['kunci_rakor'];
+?>
+<?php if($kunci_rakor == 1){?>
+    <div class="col-sm-5">
+    <button style="background-color:#88b93c; border-radius:5px; line-height:30px;"><a href="<?php echo site_url('kunci/form_pemakaian'); ?>"><b style="color:white;">BUAT VERIFIKASI</b></a></button>
+    </div>
+<?php
+}
+?>
+<br />
+<br />
+<br />
+<br />
+<div class="table-responsive">
+    <table class="table table-striped table-hover table-center" id="table-pemakaian" width="100%">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Tanggal</th>
+                <th>Tindakan</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</div>
