ac5d8ad28d68b46d2b0af3565ac02053c7d945ce
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-11-27 15:25:27 +0700

    update

--- application/modules/produksi/views/material_on_site.php
+++ application/modules/produksi/views/material_on_site.php
@@ -69,7 +69,7 @@
                     </div>
                     <div class="form-group">
                         <label>Nilai<span class="required" aria-required="true">*</span></label>
-                        <input type="text" id="total" name="total" class="form-control numberformat" autocomplete="off" required=""/>
+                        <input type="text" id="total" name="total" class="form-control nilaiformat" autocomplete="off" required=""/>
                     </div>
                     <div class="form-group">
                         <label>Satuan<span class="required" aria-required="true">*</span></label>
