05d48e9e8703deb82f81c43fba99d2890fc74e0b
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2026-06-29 14:38:28 +0700

    update

--- application/modules/admin/views/script_dashboard.php
+++ application/modules/admin/views/script_dashboard.php
@@ -385,6 +385,94 @@ foreach ($penjualan_maret26 as $x){
 }
 $realisasi_produksi_maret26 = $total_volume_penjualan_maret26;
 
+//APRIL26
+$rak_april26 = $this->db->select('(r.vol_produk_a + r.vol_produk_b + r.vol_produk_c + r.vol_produk_d + r.vol_produk_e + r.vol_produk_f) as total_produksi')
+->from('rak r')
+->where("r.tanggal_rencana_kerja between '$date_april26_awal' and '$date_april26_akhir'")
+->get()->row_array();
+$rencana_produksi_april26 = $rak_april26['total_produksi'];
+
+$penjualan_april26 = $this->db->select('p.nama, pp.client_id, SUM(pp.display_price) as price, SUM(pp.display_volume) as volume, pp.convert_measure as measure')
+->from('pmm_productions pp')
+->join('penerima p', 'pp.client_id = p.id','left')
+->join('pmm_sales_po ppo', 'pp.salesPo_id = ppo.id','left')
+->where("pp.date_production between '$date_april26_awal' and '$date_april26_akhir'")
+->where("pp.status = 'PUBLISH'")
+->where("ppo.status in ('OPEN','CLOSED')")
+->group_by("pp.client_id")
+->get()->result_array();
+$total_volume_penjualan_april26 = 0;
+foreach ($penjualan_april26 as $x){
+    $total_volume_penjualan_april26 += $x['volume'];
+}
+$realisasi_produksi_april26 = $total_volume_penjualan_april26;
+
+//MEI26
+$rak_mei26 = $this->db->select('(r.vol_produk_a + r.vol_produk_b + r.vol_produk_c + r.vol_produk_d + r.vol_produk_e + r.vol_produk_f) as total_produksi')
+->from('rak r')
+->where("r.tanggal_rencana_kerja between '$date_mei26_awal' and '$date_mei26_akhir'")
+->get()->row_array();
+$rencana_produksi_mei26 = $rak_mei26['total_produksi'];
+
+$penjualan_mei26 = $this->db->select('p.nama, pp.client_id, SUM(pp.display_price) as price, SUM(pp.display_volume) as volume, pp.convert_measure as measure')
+->from('pmm_productions pp')
+->join('penerima p', 'pp.client_id = p.id','left')
+->join('pmm_sales_po ppo', 'pp.salesPo_id = ppo.id','left')
+->where("pp.date_production between '$date_mei26_awal' and '$date_mei26_akhir'")
+->where("pp.status = 'PUBLISH'")
+->where("ppo.status in ('OPEN','CLOSED')")
+->group_by("pp.client_id")
+->get()->result_array();
+$total_volume_penjualan_mei26 = 0;
+foreach ($penjualan_mei26 as $x){
+    $total_volume_penjualan_mei26 += $x['volume'];
+}
+$realisasi_produksi_mei26 = $total_volume_penjualan_mei26;
+
+//JUNI26
+$rak_juni26 = $this->db->select('(r.vol_produk_a + r.vol_produk_b + r.vol_produk_c + r.vol_produk_d + r.vol_produk_e + r.vol_produk_f) as total_produksi')
+->from('rak r')
+->where("r.tanggal_rencana_kerja between '$date_juni26_awal' and '$date_juni26_akhir'")
+->get()->row_array();
+$rencana_produksi_juni26 = $rak_juni26['total_produksi'];
+
+$penjualan_juni26 = $this->db->select('p.nama, pp.client_id, SUM(pp.display_price) as price, SUM(pp.display_volume) as volume, pp.convert_measure as measure')
+->from('pmm_productions pp')
+->join('penerima p', 'pp.client_id = p.id','left')
+->join('pmm_sales_po ppo', 'pp.salesPo_id = ppo.id','left')
+->where("pp.date_production between '$date_juni26_awal' and '$date_juni26_akhir'")
+->where("pp.status = 'PUBLISH'")
+->where("ppo.status in ('OPEN','CLOSED')")
+->group_by("pp.client_id")
+->get()->result_array();
+$total_volume_penjualan_juni26 = 0;
+foreach ($penjualan_juni26 as $x){
+    $total_volume_penjualan_juni26 += $x['volume'];
+}
+$realisasi_produksi_juni26 = $total_volume_penjualan_juni26;
+
+//JULI26
+$rak_juli26 = $this->db->select('(r.vol_produk_a + r.vol_produk_b + r.vol_produk_c + r.vol_produk_d + r.vol_produk_e + r.vol_produk_f) as total_produksi')
+->from('rak r')
+->where("r.tanggal_rencana_kerja between '$date_juli26_awal' and '$date_juli26_akhir'")
+->get()->row_array();
+$rencana_produksi_juli26 = $rak_juli26['total_produksi'];
+
+$penjualan_juli26 = $this->db->select('p.nama, pp.client_id, SUM(pp.display_price) as price, SUM(pp.display_volume) as volume, pp.convert_measure as measure')
+->from('pmm_productions pp')
+->join('penerima p', 'pp.client_id = p.id','left')
+->join('pmm_sales_po ppo', 'pp.salesPo_id = ppo.id','left')
+->where("pp.date_production between '$date_juli26_awal' and '$date_juli26_akhir'")
+->where("pp.status = 'PUBLISH'")
+->where("ppo.status in ('OPEN','CLOSED')")
+->group_by("pp.client_id")
+->get()->result_array();
+$total_volume_penjualan_juli26 = 0;
+foreach ($penjualan_juli26 as $x){
+    $total_volume_penjualan_juli26 += $x['volume'];
+}
+$realisasi_produksi_juli26 = $total_volume_penjualan_juli26;
+
 //LABA RUGI
 //FEBRUARI25
 $total_niai_komposisi_bahan_februari25 = $this->pmm_model->getKomposisiBahan($date_februari25_awal,$date_februari25_akhir);
